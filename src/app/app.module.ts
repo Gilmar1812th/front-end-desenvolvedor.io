f722520b11ccc5a3d0eb4bc05b23d54cd8c6630e
Gilmar Santos <gilmar1812th@gmail.com>
2022-03-29 22:06:50 -0300

    Projeto_Final_desenvolvedor.io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { NavegacaoModule } from './navegacao/navegacao.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NavegacaoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
